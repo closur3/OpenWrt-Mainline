cf4d5cb2760753e62b175dab907170f384b0dd69
hsux <72607204+closur3@users.noreply.github.com>
2026-01-09 01:11:49 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -11,5 +11,3 @@
 
 #
 echo 'src-git small https://github.com/kenzok8/small' >>feeds.conf.default
-echo 'src-git argon https://github.com/jerrykuku/luci-theme-argon' >>feeds.conf.default
-echo 'src-git config https://github.com/jerrykuku/luci-app-argon-config' >>feeds.conf.default
