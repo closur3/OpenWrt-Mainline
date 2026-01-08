70e2f33171eb4a27d75f2468d459f28828bf29e7
hsux <72607204+closur3@users.noreply.github.com>
2026-01-08 23:39:08 +0800

    Add Argon theme and config sources to feeds

--- diy-part1.sh
+++ diy-part1.sh
@@ -11,3 +11,5 @@
 
 #
 echo 'src-git small https://github.com/kenzok8/small' >>feeds.conf.default
+echo 'src-git argon https://github.com/jerrykuku/luci-theme-argon' >>feeds.conf.default
+echo 'src-git config https://github.com/jerrykuku/luci-app-argon-config' >>feeds.conf.default
